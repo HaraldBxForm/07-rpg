c4e480fc69acb72944542d9f9b5c245529b917f5
HaraldIsHere <duplouis.harald@gmail.com>
2025-05-16 15:55:33 +0200

    v 2.0

--- scripts/main.js
+++ scripts/main.js
@@ -12,10 +12,11 @@
 // 🧠 Variables globales
 // ==============================
 class Chavalier {
-  constructor(name, strength, magic, life, mana, potion) {
+  constructor(name, strength, magic, maxlife, life, mana, potion) {
     this.name = name;
     this.strength = strength;
     this.magic = magic;
+    this.maxlife = maxlife;
     this.life = life;
     this.mana = mana;
     this.potion = potion;
@@ -25,24 +26,33 @@ class Chavalier {
     return (`Je suis ${this.name} et je n'aurai aucune pitié !`)
   }
 
-  attack(ennemi) {
-    ennemi.life -= this.strength;
+  attack(target) {
+    target.life -= this.strength;
+    return (`${target.name} a désormais ${target.life} ❤️`)
   }
 
-  magicAttack() {
+  magicAttack(target) {
     if (this.mana >= 20) {
-        ennemi.life -= this.magic;
+        target.life -= this.magic;
         this.mana -= 20;
+        return (`${target.name} a désormais ${target.life}`)
+    } else {
+        return `${this.name} n'a plus assez de Mana !`;
     }
   }
 
   getDamages(damage) {
-
+    this.life -= damage;
+    return `${this.name} a encore ${this.life} ❤️`;
   }
 
   usePotion() {
     if (this.potion >= 1) {
       this.life += 10;
+    //   S'assurer qu'on ne dépasse pas le max de PV
+      if (this.life > 100) {
+        this.life = 100;
+      }
       this.potion -= 1;
       return `${this.name} utilise une potion. Vie: ${this.life}, Potions restantes: ${this.potion}`;
     } else {
@@ -51,15 +61,27 @@ class Chavalier {
   }
 
   isDead() {
-    if (this.life <= 0) {
-      return;
-    }
+    return (this.life <= 0)
+  }
+
+  resetMaxLife() {
+    this.life = this.maxlife;
+    return this.life
   }
 }
 // ==============================
 // 🎊 Fonctionnalités
 // ==============================
 
+const chevalier1 = new Chavalier("Olivier", 50, 20, 100, 100, 40, 2)
+
+const chevalier2 = new Chavalier("Hugues", 30, 50, 100, 100, 20, 2)
+
+console.log(chevalier1.getDamages(10));
+console.log(chevalier1.getDamages(10));
+console.log(chevalier1.resetMaxLife());
+
 // ==============================
 // 🧲 Événements
 // ==============================
+
