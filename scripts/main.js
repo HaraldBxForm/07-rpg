c0a1b67a6faf7c245688bb5cd1af362be478e406
HaraldIsHere <duplouis.harald@gmail.com>
2025-05-20 11:55:56 +0200

    v 3.0

--- scripts/main.js
+++ scripts/main.js
@@ -3,16 +3,22 @@
 // ==============================
 
 // Inputs
+const inputHeroName = document.querySelector(`.hero-name`);
+const inputHeroCategory = document.querySelector(`.hero-category`);
 
+const choicesHero = document.querySelector(`.hero-choices`);
+const choicesTarget = document.querySelector(`.target-choices`);
+const choicesAttacks = document.querySelector(`.attack-choices`);
 // Button
-
+const addButton = document.querySelector(`.add-button`)
 // Display
+const displayPlayers = document.querySelector(`.players-container`);
 
 // ==============================
 // 🧠 Variables globales
 // ==============================
-class Chavalier {
-  constructor(name, strength, magic, maxlife, life, mana, potion) {
+class Chevalier {
+  constructor(name, strength, magic, maxlife, life, mana, potion, category) {
     this.name = name;
     this.strength = strength;
     this.magic = magic;
@@ -20,6 +26,7 @@ class Chavalier {
     this.life = life;
     this.mana = mana;
     this.potion = potion;
+    this.category = category;
   }
 
   shout() {
@@ -69,19 +76,57 @@ class Chavalier {
     return this.life
   }
 }
+
+const charactersList = []
 // ==============================
 // 🎊 Fonctionnalités
 // ==============================
 
-const chevalier1 = new Chavalier("Olivier", 50, 20, 100, 100, 40, 2)
+const chevalier1 = new Chevalier("Olivier", 50, 20, 100, 100, 40, 2, "Catégoerie")
 
-const chevalier2 = new Chavalier("Hugues", 30, 50, 100, 100, 20, 2)
+const chevalier2 = new Chevalier("Hugues", 30, 50, 100, 100, 20, 2, "Catégorie")
 
 console.log(chevalier1.getDamages(10));
 console.log(chevalier1.getDamages(10));
 console.log(chevalier1.resetMaxLife());
 
+function addCharacterToList() {
+  charactersList.push(new Chevalier(inputHeroName.value, 50, 20, 100, 100, 40, 2, inputHeroCategory.value));
+
+  const option = document.createElement(`option`);
+  option.id = option.value = inputHeroName.value;
+  option.textContent = `${inputHeroName.value} | ${inputHeroCategory.value} | ${charactersList[charactersList.length-1].life} ❤️ | ${charactersList[charactersList.length-1].strength} ⚔️`;
+
+  choicesHero.appendChild(option.cloneNode(true));
+  choicesTarget.appendChild(option);
+}
+
+function displayCharactersBoxes() {
+  displayPlayers.innerHTML = ``;
+  charactersList.forEach((element) => {
+    let div = document.createElement(`div`);
+    div.classList =`player-element`
+    div.innerHTML = `<div class="${element.name}">
+                <h2>${element.name}</h2>
+                <div>${element.category}</div>
+                <div>Force : ${element.strength}</div>
+                <div>Santé : ${element.life} ❤️</div>
+                <div>Potion : ${element.potion} ⚗️</div>
+            </div>`;
+
+    displayPlayers.appendChild(div);
+  });
+}
+
+// function displayChoicesLists
 // ==============================
 // 🧲 Événements
 // ==============================
+addButton.addEventListener(`click`, (e) => {
+  e.preventDefault();
+  addCharacterToList()
+  displayCharactersBoxes();
+  // console.log(charactersList);
+  
+})
 
